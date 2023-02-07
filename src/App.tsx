749997c19c4bc684cf72136763d63601e2d06ad4
Udaloiii <udaloiiisohappy@gmail.com>
2023-02-07 11:37:02 +0300

    Add User & Posts component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import {User} from "./user";
+import {Posts} from "./posts";
 
 function App() {
   return (
     <div className="App">
+      <User/>
+      <Posts/>
     </div>
   );
 }
